e96192ea545cdee7789d7308fe82fe1998bbe331
Ronald Reagan <reagan.r123@gmail.com>
2013-12-02 01:09:17 -0800

    Detab

--- pyac_core/core/events.py
+++ pyac_core/core/events.py
@@ -6,102 +6,102 @@ import acserver
 from core.logging import *
 
 class EventManager:
-	def __init__(self):
-		self.events = {}
-	def connect(self, event, func):
-		try:
-			self.events[event].append(func)
-		except KeyError:
-			self.events[event] = []
-			self.connect(event, func)
-	def trigger(self, eventname, args=()):
-		try:
-			for event in self.events[eventname]:
-				try:
-					event(*args)
-				except:
-					exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
-					acserver.log('Uncaught exception occured in event handler.',ACLOG_ERROR)
-					acserver.log(traceback.format_exc(),ACLOG_ERROR)
-		except KeyError:
-			pass
+    def __init__(self):
+        self.events = {}
+    def connect(self, event, func):
+        try:
+            self.events[event].append(func)
+        except KeyError:
+            self.events[event] = []
+            self.connect(event, func)
+    def trigger(self, eventname, args=()):
+        try:
+            for event in self.events[eventname]:
+                try:
+                    event(*args)
+                except:
+                    exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
+                    acserver.log('Uncaught exception occured in event handler.',ACLOG_ERROR)
+                    acserver.log(traceback.format_exc(),ACLOG_ERROR)
+        except KeyError:
+            pass
 
 class PolicyEventManager(EventManager):
-	def __init__(self):
-		EventManager.__init__(self)
-	def trigger(self, event, args=()):
-		try:
-			for event in self.events[event]:
-					if not event(*args):
-						return False
+    def __init__(self):
+        EventManager.__init__(self)
+    def trigger(self, event, args=()):
+        try:
+            for event in self.events[event]:
+                    if not event(*args):
+                        return False
 
-		except KeyError:
-			return True
-		return True
+        except KeyError:
+            return True
+        return True
 
 server_events = EventManager()
 policy_events = PolicyEventManager()
 exec_queue = []
 
 def registerServerEventHandler(event, func):
-	'''Call function when event has been executed.'''
-	server_events.connect(event, func)
+    '''Call function when event has been executed.'''
+    server_events.connect(event, func)
 
 class eventHandler(object):
-	'''Decorator which registers a function as an event handler.'''
-	def __init__(self, name):
-		self.name = name
-	def __call__(self, f):
-		self.__doc__ = f.__doc__
-		self.__name__ = f.__name__
-		registerServerEventHandler(self.name, f)
-		return f
+    '''Decorator which registers a function as an event handler.'''
+    def __init__(self, name):
+        self.name = name
+    def __call__(self, f):
+        self.__doc__ = f.__doc__
+        self.__name__ = f.__name__
+        registerServerEventHandler(self.name, f)
+        return f
 
 def triggerServerEvent(event, args):
-	'''Trigger event with arguments.'''
-	server_events.trigger(event, args)
+    '''Trigger event with arguments.'''
+    server_events.trigger(event, args)
 
 def registerPolicyEventHandler(event, func):
-	'''Call function when policy event has been executed.'''
-	policy_events.connect(event, func)
+    '''Call function when policy event has been executed.'''
+    policy_events.connect(event, func)
 
 class policyHandler(object):
-	'''Decorator which registers a function as a policy event handler.'''
-	def __init__(self, name):
-		self.name = name
-	def __call__(self, f):
-		self.__doc__ = f.__doc__
-		self.__name__ = f.__name__
-		registerPolicyEventHandler(self.name, f)
-		return f
+    '''Decorator which registers a function as a policy event handler.'''
+    def __init__(self, name):
+        self.name = name
+    def __call__(self, f):
+        self.__doc__ = f.__doc__
+        self.__name__ = f.__name__
+        registerPolicyEventHandler(self.name, f)
+        return f
 
 def triggerPolicyEvent(event, args):
-	'''Trigger policy event with arguments.'''
-	return policy_events.trigger(event, args)
+    '''Trigger policy event with arguments.'''
+    return policy_events.trigger(event, args)
 
 def execLater(func, args):
-	'''Call function at a later time with arguments in tuple args.'''
-	exec_queue.append((func, args))
+    '''Call function at a later time with arguments in tuple args.'''
+    exec_queue.append((func, args))
 
 def triggerExecQueue():
-	for event in exec_queue:
-		try:
-			event[0](*event[1])
-		except:
-			exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
-			acserver.log('Uncaught exception execLater queue.',ACLOG_WARNING)
-			acserver.log(traceback.format_exc(),ACLOG_WARNING)
-			acserver.log(traceback.extract_tb(exceptionTraceback),ACLOG_WARNING)
-	del exec_queue[:]
+    for event in exec_queue:
+        try:
+            event[0](*event[1])
+        except:
+            exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
+            acserver.log('Uncaught exception execLater queue.',ACLOG_WARNING)
+            acserver.log(traceback.format_exc(),ACLOG_WARNING)
+            acserver.log(traceback.extract_tb(exceptionTraceback),ACLOG_WARNING)
+    del exec_queue[:]
 
 @eventHandler('reload')
 def onReload():
-	server_events.events.clear()
+    server_events.events.clear()
 
 def update():
-	reactor.runUntilCurrent()
-	reactor.doIteration(0)
-	triggerExecQueue()
+    reactor.runUntilCurrent()
+    reactor.doIteration(0)
+    triggerExecQueue()
 
 reactor.startRunning()
 
