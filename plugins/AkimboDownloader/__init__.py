c0e7a030b589f22bd4d810aa1ba6f99c3888b3d6
Ronald Reagan <reagan.r123@gmail.com>
2013-12-06 18:49:07 -0800

    Adding threads to AkimboDownloader. More upgrades.

--- plugins/AkimboDownloader/__init__.py
+++ plugins/AkimboDownloader/__init__.py
@@ -1,4 +1,4 @@
-import sys, re, subprocess, requests, os
+import sys, re, subprocess, requests, os, threading, Queue
 from StringIO import StringIO
 from zipfile import ZipFile
 
@@ -8,6 +8,9 @@ from core.plugins import plugins
 from core.logging import *
 
 preferred_host = None
+download_queue = Queue.Queue()
+message_queue = Queue.Queue()
+download_thread = None
 sources = ['packages.ac-akimbo.net', 'de.ac-akimbo.net', 'us.ac-akimbo.net']
 url_string = "http://%s/packages/maps/%s.cgz.zip"
 local_maps_path = os.path.join('.','packages','maps','servermaps')
@@ -17,12 +20,14 @@ def findbesthost():
     ping_re = re.compile(ping_re)
 
     responses = {}
+    
+    acserver.log("AkimboDownloader: Checking for the best host...")
     for host in sources:
         if sys.platform == 'win32': #There is a bit different of a format for windows, and I can't get my hands on a machine to figure out everything about it and test.
             #do ping command here
             return sources[0]
         else:
-            ping = subprocess.Popen(["ping", "-c1", host], stdout=subprocess.PIPE).stdout.read()
+            ping = subprocess.Popen(["ping", "-c1", "-s2048", host], stdout=subprocess.PIPE).stdout.read()
             m = ping_re.search(ping)
             if m != None:
                 avg = m.groupdict()['avg']
@@ -50,18 +55,16 @@ def downloadMap(mapname):
 
 @eventHandler('serverExtension')
 def serverext(cn,ext,ext_text):
+    global download_thread
     if ext == "getmap":
         if auth.hasPermission(cn, "getMap"):
             maps = ext_text.split()
             acserver.log("AkimboDownloader: Download request from user %s cn(%d), maps: %s"%(auth.AuthenticatedClients[cn].name,cn,",".join(maps)))
-            for mapname in maps:
-                rcode = downloadMap(mapname)
-                if rcode != 200:
-                    acserver.log("AkimboDownloader: Error downloading %s, status code: %d"%(mapname,rcode),ACLOG_ERROR)
-                    acserver.msg("\f3Error downloading %s, status code: %d"%(mapname,rcode),cn)
-                else:
-                    acserver.log("AkimboDownloader: Downloaded map %s"%(mapname))
-                    acserver.msg("\f2Downloaded map %s successfuly!"%(mapname),cn)
+            acserver.msg("\f2Attempting to download: %s"%(",".join(maps)),cn)
+            
+            for m in maps:
+                download_queue.put((m,cn))
+                
         else:
             acserver.msg("\f3You don't have access to that command!",cn)
     
@@ -88,10 +91,28 @@ def serverext(cn,ext,ext_text):
         else:
             acserver.msg("\f3You don't have access to that command!",cn)
 
+def downloadThreadFunc():
+    global download_queue, message_queue
+    while True:
+        mapname,cn = download_queue.get()
+        
+        rcode = downloadMap(mapname)
+        if rcode != 200:
+            message_queue.put({'text':"AkimboDownloader: Error downloading %s, status code: %d"%(mapname,rcode),'type':'log'})
+            message_queue.put({'text':"\f3Error downloading %s, status code: %d"%(mapname,rcode),'cn':cn, 'type':'msg'})
+        else:
+            message_queue.put({'text':"AkimboDownloader: Downloaded map %s"%(mapname),'type':'log'})
+            message_queue.put({'text':"\f2Downloaded map %s successfuly!"%(mapname),'cn':cn, 'type':'msg'})
+        
+        download_queue.task_done()
+
 #Lets make the plugin loader happy.
 def main(plugin):
-    global preferred_host
+    global preferred_host, download_thread
     preferred_host = findbesthost()
+    
+    download_thread = threading.Thread(None, target=downloadThreadFunc)
+    download_thread.start()
     acserver.log("AkimboDownloader: Setting preferred host to: %s"%preferred_host)
 
 @eventHandler('initEnd')
@@ -99,4 +120,17 @@ def initend():
     global auth
     auth = plugins['Authentication'].module
     auth.addPermissionIfMissing("getMap","Allows a user to make the server download a map from akimbo. Warning: This is non-threaded and will cause the server to hang.")
-    auth.addPermissionIfMissing("rmMap","Allows a user to delte a map from servermaps/")
+    auth.addPermissionIfMissing("rmMap","Allows a user to delte a map from servermaps.")
+
+@eventHandler('serverTick')
+def servertick(gm,sm):
+    try:
+        msg = message_queue.get(False)
+    except Queue.Empty:
+        return
+    
+    if msg['type'] == 'log':
+        acserver.log(msg['text'])
+    elif msg['type'] == 'msg':
+        acserver.msg(msg['text'], msg['cn'])
+        
