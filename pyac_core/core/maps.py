8d25c74672c3b6a57f2573976fcdbe2f960b6b38
Ronald Reagan <reagan.r123@gmail.com>
2013-12-13 14:32:32 -0800

    core.maps: Adding folder paths.

--- pyac_core/core/maps.py
+++ pyac_core/core/maps.py
@@ -1,17 +1,21 @@
 import os
 
+OfficalMapPath = os.path.join('.','packages','maps','official')
+ServerMapPath = os.path.join('.','packages','maps','servermaps')
+IncomingMapPath = os.path.join('.','packages','maps','servermaps','incoming')
+
 OfficialMaps = []
 ServerMaps = []
 IncomingMaps = []
 
-for i in os.listdir(os.path.join('.','packages','maps','official')):
+for i in os.listdir(OfficalMapPath):
     if i.endswith('.cgz'):
         OfficialMaps.append(i[:-4])
 
-for i in os.listdir(os.path.join('.','packages','maps','servermaps')):
+for i in os.listdir(ServerMapPath):
     if i.endswith('.cgz'):
         ServerMaps.append(i[:-4])
 
-for i in os.listdir(os.path.join('.','packages','maps','servermaps','incoming')):
+for i in os.listdir(IncomingMapPath):
     if i.endswith('.cgz'):
         IncomingMaps.append(i[:-4])
